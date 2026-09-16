28445b4ef20ab0376b364380de7a364dc67d1758
Muhammad Kamran <mkamran.bscssef20@iba-suk.edu.pk>
2026-09-16 19:42:19 +0500

    Initial commit

---
+++ src/test/java/com/kamran/ECommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamran.ECommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
